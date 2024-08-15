320184feff3398cab2a1f6281b71497418d49c56
Micheal Parks <micheal.parks@outlook.com>
2024-08-15 12:43:55 -0400

    docs

---
+++ packages/docs/src/components/tabs.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Tabs } from '@viamrobotics/prime-core';
+
+  let tab = 'Tab 1';
+</script>
+
+<div class="flex w-full flex-col gap-4">
+  <Tabs
+    cx="bg-cyan-500 w-full"
+    tabs={['Tab 1', 'Tab 2', 'Tab 3']}
+    selected={tab}
+    on:input={(event) => {
+      tab = event.detail.value;
+    }}
+  />
+  <p>Hello From {tab}!</p>
+</div>
